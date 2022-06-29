b7da2eae51edb3a93c6607eb19a90cb382ad5d4d
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-06-29 10:37:38 +0530

    Schema added

---
+++ src/model/movie.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema({
+    MovieName : {type:String},
+    Rating : {type:Number},
+    Cast : {type:Array},
+    Genre : {type:String},
+    ReleaseDate : {type:Date},
+},{
+    versionkey: false,
+    timestamps: true
+})
+
+module.exports = mongoose.model("movie", movieSchema)
