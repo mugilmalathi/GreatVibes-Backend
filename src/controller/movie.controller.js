e43d2cfde7907f8c353ca6ea42b80f685e97b3b2
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-06-29 11:52:15 +0530

    full flow completed

--- src/controller/movie.controller.js
+++ src/controller/movie.controller.js
@@ -6,11 +6,7 @@ router.post("/movies", async(req, res)=>{
 
     try{
         const movies = await Movie.create(req.body);
-        if(movies){
-            return res.send("Already Movie Exist.!")
-        }else{
-            return res.status(201).send(movies)
-        }
+        return res.status(201).send(movies)
     }catch(err){
         return res.status(500).send({message: err.message})
     }
